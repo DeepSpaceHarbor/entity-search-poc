9937c8191d97f29a41d213385bd8d92a3d8770fb
“Borche” <hello@borche.dev>
2026-02-10 22:50:34 +0100

    Clean state

--- src/lib/client/init.ts
+++
@@ -1,2 +0,0 @@
-// FyonUI
-import('flyonui/flyonui');
--- src/lib/utils/isValidJson.ts
+++
@@ -1,8 +0,0 @@
-export function isValidJson(input: string) {
-	try {
-		const parsed = JSON.parse(input);
-		return parsed;
-	} catch {
-		return false;
-	}
-}
